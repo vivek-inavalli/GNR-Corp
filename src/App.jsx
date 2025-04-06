4540ab9571b64fc29abdcfcb7296099ae11eb582
vivek <inavallivivek6@gmail.com>
2025-04-06 14:26:07 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import SlidingGallery from "./components/SlidingGallery";
@@ -7,21 +8,37 @@ import EventHighlights from "./components/EventHighlights";
 import ContactInfo from "./components/ContactInfo";
 import RegisterSection from "./components/RegisterSection";
 import Footer from "./components/Footer";
+import Register from "./components/Register";
+import EventInfo from "./components/EventInfo";
+import ContactAs from "./components/ContactAs";
+import AboutAs from "./components/AboutAs";
+
+const Home = () => {
+  return (
+    <div className="pt-24">
+      <HeroSection />
+      <SlidingGallery />
+      <SecondHero />
+      <EventHighlights />
+      <ContactInfo />
+      <RegisterSection />
+      <Footer />
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <div className="pt-[90px]">
-        <HeroSection />
-        <SlidingGallery />
-        <SecondHero />
-        <EventHighlights />
-        <ContactInfo />
-        <RegisterSection />
-        <Footer />
-      </div>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/eventinfo" element={<EventInfo />} />
+        <Route path="/aboutAs" element={<AboutAs />} />
+        <Route path="/contactAs" element={<ContactAs />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/AboutAs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutAs = () => {
+  return <div className="pt-24">AboutAs</div>;
+};
+
+export default AboutAs;
